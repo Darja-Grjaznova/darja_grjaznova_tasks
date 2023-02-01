0ee061dc51c50d3bc5106c75715e47632bce5933
DariaMuzichenko <darjadg12@gmail.com>
2023-02-01 10:29:21 +0200

    Initial commit

---
+++ ex1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First PHP Page</title>
+</head>
+<body>
+    <h1>3.1 Write a simple PHP script</h1>
+    <?php
+    echo "Darja Grjaznova<br>";
+    echo "BBCAP22";
+    ?>
+    <h1>3.2 Write PHP code to display </h1>
+    <?php
+    echo "Hello world! My name is \"Darja\"";
+    ?>
+    <h1>3.3 display the current date </h1>
+    <?php
+    echo date("d.m.Y")
+    ?>
+    <h1>3.4 $title = "PHP is interesting”. Put this </h1>
+    <?php
+    
+    ?>
+
+</body>
+</html>
